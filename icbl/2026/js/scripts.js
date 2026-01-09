3658a5e527bdd1e231c5dd12f404aaa2ff9a7495
lichenrichard <richard_lichen@hotmail.com>
2026-01-09 09:34:43 +0800

    added CMT link

--- icbl/2026/js/scripts.js
+++ icbl/2026/js/scripts.js
@@ -57,7 +57,7 @@ function loadComponents() {
             <li><a href="organization.html">Organization</a></li>
             <li><a href="#">Committee</a></li>          
             <li><a href="contact.html">Contacts</a></li>
-            <li><a href="index.html#content" class="link-btn">Call for Papers</a></li>
+            <li><a href="https://cmt3.research.microsoft.com/ICBL2026" target="_blank" class="link-btn">Paper Submission</a></li>
         </ul>
     </nav>
     <div id='menu' class='box-icon'><i class='bx bx-menu'></i></div>
